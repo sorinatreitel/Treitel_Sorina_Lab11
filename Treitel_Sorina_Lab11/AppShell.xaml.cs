ccf37431998761bd579de48b949bbb2c6a2dd554
sorinatreitel <sorinatreitel@yahoo.ro>
2022-12-20 17:07:05 +0200

    Laborator11

---
+++ Treitel_Sorina_Lab11/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Treitel_Sorina_Lab11
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Treitel_Sorina_Lab11/Data/IRestService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks;
+using Treitel_Sorina_Lab11.Models;
+namespace Treitel_Sorina_Lab11.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
